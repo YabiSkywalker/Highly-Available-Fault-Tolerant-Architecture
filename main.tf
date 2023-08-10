625b086ba2f68b747acd88a0bd69e789d745a356
Yabi <gebrekidanyabsera@gmail.com>
2023-08-10 14:51:11 -0500

    second commit

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # location of where the resources will be deployed 
 provider "aws" {
-  region = "us-east-1"
+  region = "vars.aws_region"
 }
 resource "aws_instance" "my_instance" {
   ami           = data.aws_ami.ubuntu.id
